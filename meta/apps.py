50ceee0a3e44120b6ee276566803bc7a43c28323
Redish101 <jiayunluo@outlook.com>
2022-04-13 15:34:13 +0800

    F: 优化了默认主题

--- meta/apps.py
+++ meta/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class metaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'meta'
+    verbose_name = '设置'
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from posts.models import Post
 from meta.models import SiteMeta
+from django.http import HttpResponse
 
 
-def home(request):
-    posts=Post.objects.all()
-    meta=SiteMeta.objects.all()
+def home(request,):
+    posts = Post.objects.all()
+    meta = SiteMeta.objects.all()
+    context = {
+        'posts': posts,
+        'meta': meta,
+    }
+    return render(request, 'home.html', context)
 
-    return render(request, 'home.html', {'meta': meta, 'posts': posts})
+
+def post(request, id):
+    meta = SiteMeta.objects.all()
+    post = Post.objects.get(id=id)
+    context = {
+        'name': post.name,
+        'body': post.body
+    }
+    return render(request, post.html, context)
